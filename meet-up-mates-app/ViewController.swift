9de0342caa59a96d090a001e343118b26cedbb56
Priyanka <priyanka.sahas@live.com>
2015-03-25 15:28:31 +1100

    Initial Commit

---
+++ meet-up-mates-app/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  meet-up-mates-app
+//
+//  Created by Priyanka Patel on 25/03/2015.
+//  Copyright (c) 2015 Priyanka Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func signIn(sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
